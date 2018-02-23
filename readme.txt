4249dcab792c5b2863cfa81be9ed29e97c90df97
GITUSER <hudson@inboundnow.com>
2018-02-23 01:43:10 -0600

    1.1.5  https://github.com/atwellpub/resend-welcome-email/issues/16

---
+++ readme.txt
@@ -0,0 +1,73 @@
+=== Resend Welcome Email ===
+Contributors: adbox, ramiy, jazbek, afragen, titus
+Donate link: hudson.atwell@gmail.com
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Tags: users, welcome-email, user-management, support
+Requires at least: 4.3.1
+Tested up to: 4.9.4
+Stable Tag: 1.1.5
+Requires PHP: 5.3
+
+Quickly send a new welcome email and password reset link for a user through the user's profile edit area.
+
+== Description ==
+
+This tool was developed to quickly send a user a new password reset link via email when they are having trouble logging in.
+
+= Developers & Designers =
+
+This extension has a public GitHub page where users can contribute fixes and improvements.
+
+[Follow Development on GitHub](https://github.com/atwellpub/resend-welcome-email "Follow & Contribute to core development on GitHub")
+
+[Follow Developer on Twitter](https://twitter.com/atwellpub "Follow the developer on Twitter")
+
+= Contributors =
+
+[Tibor Repček](https://github.com/tiborepcek/ "Tibor Repček on GitHub") - translation into slovak language (slovenčina)
+
+== Installation ==
+
+1. Upload `resend-welcome-email` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= Do you provide support for this plugin =
+
+Not really. Please open an issue on GitHub though if you have a problem. I am keeping track with pull requests so fire away.
+
+== Changelog ==
+
+= 1.1.5 =
+* Improving labels
+
+= 1.1.3 =
+* Updating 'Tested up to' in readme.txt
+
+= 1.1.2 =
+* Adding language files sk_SK.po, sk_SK.mo
+
+= 1.1.1 =
+* Adding resend welcome email to user row action link.
+* Converting edit_user to edit_users to fix soft error.
+
+= 1.1.0 =
+* Security: Escape translated strings.
+* Refactor.
+* Fix: Logic in notice.
+* Add: Multisite compatibility.
+
+= 1.0.3 =
+* Security: Prevent direct access to php files.
+* Security: Prevent direct access to directories.
+* i18n: Use [translate.wordpress.org](https://translate.wordpress.org/) to translate the plugin to other languages.
+
+= 1.0.2 =
+
+* wp_new_user_notification() stopped sending passwords via email, and instead it sends a reset password link.
+
+= 1.0.1 =
+
+* Initial release.
---
+++ resend-welcome-email.php
@@ -0,0 +1,192 @@
+<?php
+/*
+Plugin Name: Resend Welcome Email
+Plugin URI:  http://www.twitter.com/atwellpub
+Description: Quickly send a new welcome email and password reset link for a user through the user's profile edit area.
+Version:     1.1.5
+Author:      Hudson Atwell
+Author URI:  https://codeable.io/developers/hudson-atwell/?ref=99TG1
+Text Domain: resend-welcome-email
+Domain Path: /assets/lang
+*/
+
+
+/**
+ * Security check.
+ * Prevent direct access to the file.
+ *
+ * @since 1.0.3
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Resend_Welcome_Email
+ *
+ * @since 1.0.0
+ */
+if ( ! class_exists( 'Resend_Welcome_Email' ) ) {
+
+	/**
+	 * Class Resend_Welcome_Email
+	 */
+	class Resend_Welcome_Email {
+
+		/**
+		 * Constructor.
+		 */
+		public function __construct() {
+
+			/* Check user permission */
+			if ( ! current_user_can( 'edit_users' ) ) {
+				return;
+			}
+
+			/* Define constants */
+			//self::define_constants();
+
+			add_filter( 'user_row_actions',  array( __CLASS__, 'filter_user_row_actions' ), 10, 2 );
+			add_filter( 'personal_options', array( __CLASS__, 'personal_options' ), 10, 2 );
+
+			/* Load plugin translation files */
+			add_action( 'plugins_loaded', array( $this, 'load_textdomain' ) );
+
+			/* Adds admin listeners for processing actions */
+			self::add_admin_listeners();
+		}
+
+		/**
+		 *  Defines constants.
+		 */
+		public static function define_constants() {
+			define( 'RESEND_WELCOME_EMAIL_CURRENT_VERSION', '1.1.5' );
+			define( 'RESEND_WELCOME_EMAIL_LABEL', 'Resend Welcome Email' );
+			define( 'RESEND_WELCOME_EMAIL_SLUG', plugin_basename( dirname( __FILE__ ) ) );
+			define( 'RESEND_WELCOME_EMAIL_FILE', __FILE__ );
+			define( 'RESEND_WELCOME_EMAIL_URLPATH', plugins_url( ' ', __FILE__ ) );
+			define( 'RESEND_WELCOME_EMAIL_PATH', WP_PLUGIN_DIR . '/' . plugin_basename( dirname( __FILE__ ) ) . '/' );
+		}
+
+		/**
+		 * Discovers which tests to run and runs them.
+		 *
+		 * @param array    $actions
+		 * @param \WP_User $user
+		 *
+		 * @return array
+		 */
+		public static function filter_user_row_actions( array $actions, WP_User $user ) {
+			if ( ! ( $link = self::send_welcome_email_url( $user ) ) ) {
+				return $actions;
+			}
+
+			$actions['send_welcome_email'] = '<a href="' . $link . '">' . esc_html__( 'Resend Welcome Email', 'resend-welcome-email' ) . '</a>';
+
+			return $actions;
+		}
+
+		/**
+		 * Add link in user profile.
+		 *
+		 * @param \WP_User $user
+		 */
+		public static function personal_options( WP_User $user ) {
+			if ( ! ( $link = self::send_welcome_email_url( $user ) ) ) {
+				return;
+			}
+
+			?>
+			<tr>
+				<th scope="row"><?php esc_html_e( 'Welcome Email', 'resend-welcome-email' ); ?></th>
+				<td>
+					<a href="<?php echo $link; ?>"><?php esc_html_e( 'Send New', 'resend-welcome-email' ); ?></a>
+				</td>
+			</tr>
+			<?php
+		}
+
+		/**
+		 * Listens for email send commands and fires them.
+		 */
+		public static function add_admin_listeners() {
+			if ( ! isset( $_GET['action'] ) ||
+			     ( 'resend_welcome_email' !== $_GET['action'] )
+			) {
+				return;
+			}
+
+			/* Resend welcome email */
+			self::resend_welcome_email();
+
+			/* Register success notice */
+			add_action( 'admin_notices', array( __CLASS__, 'define_notice' ) );
+			add_action( 'network_admin_notices', array( __CLASS__, 'define_notice' ) );
+		}
+
+		/**
+		 * Register admin notice that email has been sent.
+		 */
+		public static function define_notice() {
+			?>
+			<div class="updated">
+				<p><?php esc_html_e( 'Welcome email sent!', 'resend-welcome-email' ); ?></p>
+			</div>
+			<?php
+		}
+
+		/**
+		 * Helper function. Returns the switch to or switch back URL for a given user.
+		 *
+		 * @param  WP_User $user The user to be switched to.
+		 *
+		 * @return string|bool The required URL, or false if there's no old user or the user doesn't have the required capability.
+		 */
+		public static function send_welcome_email_url( WP_User $user ) {
+			return esc_url( wp_nonce_url( add_query_arg( array(
+					'action'  => 'resend_welcome_email',
+					'user_id' => $user->ID,
+				), '' ),
+					"send_welcome_email_{$user->ID}" )
+			);
+		}
+
+		/**
+		 * Resends the welcome email.
+		 *
+		 * @return bool|WP_User WP_User object on success, false on failure.
+		 */
+		public static function resend_welcome_email() {
+			if ( ! isset( $_GET['user_id'] ) ) {
+				return false;
+			}
+
+			$user_id = $_GET['user_id'];
+
+			if ( ! $user = get_userdata( $user_id ) ) {
+				return false;
+			}
+
+			wp_new_user_notification( $user_id, null, 'both' );
+		}
+
+		/**
+		 * Load the text domain for translation.
+		 *
+		 * since: 1.0.3
+		 */
+		public function load_textdomain() {
+			load_plugin_textdomain( 'resend-welcome-email' );
+		}
+
+	}
+
+	/**
+	 *  Load Resend_Welcome_Email class in init.
+	 */
+	function Load_Resend_Welcome_Email() {
+		new Resend_Welcome_Email();
+	}
+
+	add_action( 'admin_init', 'Load_Resend_Welcome_Email', 99 );
+}
